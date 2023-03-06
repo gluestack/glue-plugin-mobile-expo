71493908bdbeffd37aadbcd42755da07698f81e0
Faiz A. Farooqui <faizz.af@gmail.com>
2023-03-06 13:38:42 +0530

    chore: init react native with expo

---
+++ dist/src/PluginInstanceContainerController.d.ts
@@ -0,0 +1,26 @@
+import IApp from "@gluestack/framework/types/app/interface/IApp";
+import IInstance from "@gluestack/framework/types/plugin/interface/IInstance";
+import IContainerController from "@gluestack/framework/types/plugin/interface/IContainerController";
+export declare class PluginInstanceContainerController implements IContainerController {
+    app: IApp;
+    status: "up" | "down";
+    portNumber: number;
+    containerId: string;
+    callerInstance: IInstance;
+    constructor(app: IApp, callerInstance: IInstance);
+    getCallerInstance(): IInstance;
+    getEnv(): string;
+    getDockerJson(): {
+        name: string;
+    };
+    getStatus(): "up" | "down";
+    getPortNumber(): number;
+    getContainerId(): string;
+    setStatus(status: "up" | "down"): "up" | "down";
+    setPortNumber(portNumber: number): number;
+    setContainerId(containerId: string): string;
+    getConfig(): any;
+    up(): Promise<void>;
+    down(): Promise<void>;
+    build(): Promise<void>;
+}
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+//@ts-ignore
+import packageJSON from "../package.json";
+import { Workspaces } from "@gluestack/helpers";
+import { PluginInstance } from "./PluginInstance";
+import IApp from "@gluestack/framework/types/app/interface/IApp";
+import IPlugin from "@gluestack/framework/types/plugin/interface/IPlugin";
+import IInstance from "@gluestack/framework/types/plugin/interface/IInstance";
+import ILifeCycle from "@gluestack/framework/types/plugin/interface/ILifeCycle";
+import IManagesInstances from "@gluestack/framework/types/plugin/interface/IManagesInstances";
+import IGlueStorePlugin from "@gluestack/framework/types/store/interface/IGluePluginStore";
+
+import { reWriteFile } from './helpers/rewrite-file';
+
+// Do not edit the name of this class
+export class GlueStackPlugin implements IPlugin, IManagesInstances, ILifeCycle {
+  app: IApp;
+  instances: IInstance[];
+	type: 'stateless' | 'stateful' | 'devonly' = 'stateless';
+  gluePluginStore: IGlueStorePlugin;
+
+  constructor(app: IApp, gluePluginStore: IGlueStorePlugin) {
+    this.app = app;
+    this.instances = [];
+    this.gluePluginStore = gluePluginStore;
+  }
+
+  init() {
+    //
+  }
+
+  destroy() {
+    //
+  }
+
+  getName(): string {
+    return packageJSON.name;
+  }
+
+  getVersion(): string {
+    return packageJSON.version;
+  }
+
+  getType(): 'stateless' | 'stateful' | 'devonly' {
+    return this.type;
+  }
+
+  getTemplateFolderPath(): string {
+    return `${process.cwd()}/node_modules/${this.getName()}/template`;
+  }
+
+  getInstallationPath(target: string): string {
+    return `./${target}`;
+  }
+
+  async runPostInstall(instanceName: string, target: string) {
+    const instance: PluginInstance = await this.app.createPluginInstance(
+      this,
+      instanceName,
+      this.getTemplateFolderPath(),
+      target,
+    );
+
+    if (!instance) {
+      return;
+    }
+
+    // update package.json'S name index with the new instance name
+    const pluginPackage = `${instance.getInstallationPath()}/package.json`;
+    await reWriteFile(pluginPackage, instanceName, 'INSTANCENAME');
+
+    // update root package.json's workspaces with the new instance name
+    const rootPackage = `${process.cwd()}/package.json`;
+    await Workspaces.append(rootPackage, instance.getInstallationPath());
+  }
+
+  createInstance(
+    key: string,
+    gluePluginStore: IGlueStorePlugin,
+    installationPath: string,
+  ): IInstance {
+    const instance = new PluginInstance(
+      this.app,
+      this,
+      key,
+      gluePluginStore,
+      installationPath,
+    );
+    this.instances.push(instance);
+    return instance;
+  }
+
+  getInstances(): IInstance[] {
+    return this.instances;
+  }
+}
